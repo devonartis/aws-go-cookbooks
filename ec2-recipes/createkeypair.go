e62e8e6b33b5408c9c357b557e1c6e1d5a13399a
Troin Artis <devon@devonartis.com>
2018-03-20 22:39:47 -0400

    Updated createkeypair.go with flags for cli

--- ec2-recipes/createkeypair.go
+++ ec2-recipes/createkeypair.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
+	"runtime"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
@@ -16,6 +19,10 @@ var (
 	svc      *ec2.EC2
 )
 
+const info = `
+          Application %s starting.
+		  The binary was build by GO: %s`
+
 func exitErrorf(msg string, args ...interface{}) {
 	fmt.Fprintf(os.Stderr, msg+"\n", args...)
 	os.Exit(1)
@@ -27,15 +34,41 @@ func savePem(f string, k string) error {
 
 }
 
-
-
 func main() {
-	//pairname := flag.String("Keyname", "~/.ssh/", "Enter the name of the")
-	pairname = "testWB"
-	svc = ec2.New(session.New(&aws.Config{Region: aws.String("us-west-1")}))
+
+	log.Printf(info, "Creating Key Pair: ", runtime.Version())
+
+	/*
+		TO DO: pairname should not be default of empty string
+		add additional value checking for the pairname
+		think about adding os.args with flags
+	*/
+
+	//flags for CLI commands
+
+	pairname := flag.String("keyname", "", "Enter keypair name to create")
+	profile := flag.String("profile", "", "Default profile will be used")
+	region := flag.String("region", "us-east-1", "Region defaults to us-east-2")
+
+	flag.Parse()
+
+	// Create a new AWS Session with Options based on if a profile was given
+
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		Config:  aws.Config{Region: aws.String(*region)},
+		Profile: *profile,
+	}))
+
+	//Get a handle on EC2 Service
+
+	svc := ec2.New(sess)
+
+	if *pairname == "" {
+		exitErrorf("Keyname can not be empty")
+	}
 
 	keyresult, err := svc.CreateKeyPair(&ec2.CreateKeyPairInput{
-		KeyName: aws.String(pairname),
+		KeyName: aws.String(*pairname),
 	})
 
 	if err != nil {
@@ -45,7 +78,7 @@ func main() {
 		exitErrorf("Unable to create key pair: %s, %v.", pairname, err)
 	}
 
-	savePem(pairname+".pem", *keyresult.KeyMaterial)
+	savePem(*pairname+".pem", *keyresult.KeyMaterial)
 
 	fmt.Printf("Created key pair %q %s\n%s\n",
 		*keyresult.KeyName, *keyresult.KeyFingerprint,
